2e2842918a331b67498b1ab516ef7fc3cef50fa9
Mike Lonergan <mikethecanuck@gmail.com>
2018-06-02 13:26:23 -0700

    First step - have implemented routing for local-elections to enable container to match AWS configuration

--- elections_api/urls.py
+++ elections_api/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Hack Oregon 2018 Local Elections APIs')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('api.urls')),
+    url(r'^local-elections/$', schema_view),
 ]
