4ee9e78050ca0a7f1eb10b564933ceaf483ac453
Mike Lonergan <mikethecanuck@gmail.com>
2018-06-02 16:02:15 -0700

    remove unneded route

--- elections_api/urls.py
+++ elections_api/urls.py
@@ -7,7 +7,6 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Hack Oregon 2018 Local Elections APIs')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^', include('api.urls')),
     url(r'^local-elections/$', schema_view),
 ]
